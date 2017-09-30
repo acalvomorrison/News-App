f71a6a66f57b62ca5ed0d9adece94343714d20fe
Andrés Calvo Morrison <acalvomorrison@gmail.com>
2017-09-30 15:32:20 -0400

    Initial Commit

---
+++ News VenezuelaTests/News_VenezuelaTests.swift
@@ -0,0 +1,36 @@
+//
+//  News_VenezuelaTests.swift
+//  News VenezuelaTests
+//
+//  Created by Andres Calvo on 9/30/17.
+//  Copyright © 2017 Andres Calvo. All rights reserved.
+//
+
+import XCTest
+@testable import News_Venezuela
+
+class News_VenezuelaTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
