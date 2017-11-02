f80e9c5ef05a934ea1792950b4afb84340cd1861
Andres Calvo Morrison <32209923+acalvomorrison@users.noreply.github.com>
2017-11-02 17:33:02 -0400

    First commit

--- News Venezuela/FirstViewController.swift
+++ News Venezuela/FirstViewController.swift
@@ -7,19 +7,55 @@
 //
 
 import UIKit
+import SwiftyXMLParser
 
-class FirstViewController: UIViewController {
+//extension String {
+//    func toJSON() -> Any? {
+//        guard let data = self.data(using: .utf8, allowLossyConversion: false) else { return nil }
+//        return try? JSONSerialization.jsonObject(with: data, options: .mutableContainers)
+//    }
+//}
 
+class FirstViewController: UIViewController, NSURLConnectionDelegate {
+    
+    var data = NSMutableData()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        var request = URLRequest(url: URL(string: "http://eltiempo.com.ve/rss/global")!)
 
+
+        request.httpMethod = "GET"
+
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            guard let data = data, error == nil else {                                                 // check for fundamental networking error
+                print("error=\(String(describing: error))")
+                return
+            }
+
+            if let httpStatus = response as? HTTPURLResponse, httpStatus.statusCode != 200 {           // check for http errors
+                print("statusCode should be 200, but is \(httpStatus.statusCode)")
+                print("response = \(String(describing: response))")
+            }
+
+            let responseString = String(data: data, encoding: .utf8)
+            print(responseString!)
+
+//            let json: AnyObject? = responseString?.toJSON() as AnyObject
+//            print("Parsed JSON: \(json!)")
+//            let xml = XMLParser.init(data: data)
+//            print(xml)
+
+        }
+        task.resume()
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-
 }
-
---
+++ Podfile
@@ -0,0 +1,23 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+pod "SwiftyXMLParser", :git => 'https://github.com/yahoojapan/SwiftyXMLParser.git'
+pod "AlamofireXmlToObjects"
+
+target 'News Venezuela' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for News Venezuela
+
+  target 'News VenezuelaTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'News VenezuelaUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
